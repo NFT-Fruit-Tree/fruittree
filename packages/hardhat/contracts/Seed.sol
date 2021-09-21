2a59cb34b6466ff6216b4ba75c1d313b54bfeb34
Wary <one.should.be.wary@gmail.com>
2021-09-21 18:00:30 +0200

    :construction: water fertilize move and burn functions of the seed contract implemented

--- packages/hardhat/contracts/Seed.sol
+++ packages/hardhat/contracts/Seed.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.4;
 
 import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { ERC721Enumerable } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { Land } from "./Land.sol";
 
 /// You are not authorized to do this action
@@ -15,7 +16,7 @@ error TransferError(string symbol, address from, address to, uint256 amount);
 /**
  * The Seed contract of the `nft-fruit-tree` game.
  */
-contract Seed is ERC721 {
+contract Seed is ERC721, ERC721Enumerable {
 
     enum TreeState {
         SEED, 
@@ -44,7 +45,7 @@ contract Seed is ERC721 {
     // The Land contract
     Land immutable land;
     // The interest bearing token used as fertilizer
-    IERC20 immutable fertilizer;
+    ERC20 immutable fertilizer;
 
     event Planted(uint256 indexed seedId, uint256 indexed landId);
     event Watered(uint256 indexed seedId);
@@ -52,10 +53,23 @@ contract Seed is ERC721 {
     event Harvested(uint256 indexed seedId, uint256 fruitAmount);
     /// The seed `id` is already planted
     error AlreadyPlantedError();
+    /// The state `state` is invalid
+    error InvalidStateError(TreeState state);
 
     constructor(address _landAddress, address _fertilizerAddress) ERC721("Seed", "SEED") {
         land = Land(_landAddress);
-        fertilizer = IERC20(_fertilizerAddress);
+        fertilizer = ERC20(_fertilizerAddress);
+    }
+
+    function getWaterLevel(uint256 _treeId) public view returns (uint256) {
+        uint256 lastWatered = treeDatas[_treeId].lastWatered;
+        uint8 dehydrateRate = treeDatas[_treeId].dehydrateRate;
+        int256 waterLevel = int(100 - dehydrateRate * (block.timestamp - lastWatered));
+        return waterLevel < 0 ? 0 : uint256(waterLevel);
+    }
+
+    function getState(uint256 _seedId) public view returns (TreeState) {
+
     }
 
     /**
@@ -69,8 +83,8 @@ contract Seed is ERC721 {
      * It will initialize its TreeData.
      */
     function plant(uint256 _seedId, uint256 _landId) external {
-        // Check if the sender owns the seed AND the land
-        if (ownerOf(_seedId) != msg.sender || Land.ownerOf(_landId) != msg.sender) revert Unauthorized();
+        // Check if the token exists and if the sender owns the seed AND the land
+        if (ownerOf(_seedId) != msg.sender || land.ownerOf(_landId) != msg.sender) revert Unauthorized();
         // Check if the seed is not already planted
         if (treeDatas[_seedId].landId != 0) revert AlreadyPlantedError();
         // Transfer the amount of fertilizer from the sender to this contract
@@ -84,13 +98,14 @@ contract Seed is ERC721 {
         emit Fertilized(_seedId, treeDatas[_seedId].fertilizerAmount);
         emit Watered(_seedId);
     }
+
     /**
      * Fertilize one of `your` `alive` trees with some token to boost its fruit production capacity.
      * @dev It is somewhat similar to staking some token for an unlimited time.
      * It will persist the new state data.
      */
     function fertilize(uint256 _treeId) external {
-        // Check if the sender owns the tree
+        // Check if the token exists and if the sender owns the tree
         if (ownerOf(_treeId) != msg.sender) revert Unauthorized();
         // Transfer the amount of fertilizer from the sender to this contract
         if (!fertilizer.transferFrom(msg.sender, address(this), treeDatas[_treeId].fertilizerAmount)) revert TransferError(fertilizer.symbol(), msg.sender, address(this), treeDatas[_treeId].fertilizerAmount);
@@ -100,9 +115,19 @@ contract Seed is ERC721 {
         emit Fertilized(_treeId, treeDatas[_treeId].fertilizerAmount);
     }
     /**
-     * Water one of `your` `alive` trees to maintain it `alive`. A dead tree cannot grow or produce fruit anymore.
+     * Water one of `your` `alive` `trees` to maintain it `alive`. A dead tree cannot grow or produce fruit anymore.
      */
-    function water() external {}
+    function water(uint256 _treeId) external {
+        // Check if the token exists and if the sender owns the tree
+        if (ownerOf(_treeId) != msg.sender) revert Unauthorized();
+        // Check if it is not a seed or dead
+        TreeState treeState = getState(_treeId);
+        if (treeState == TreeState.SEED || treeState == TreeState.DEAD) revert InvalidStateError(treeState);
+        // Update the tree state
+        treeDatas[_treeId].lastWatered = block.timestamp;
+        // Event
+        emit Watered(_treeId);
+    }
     /**
      * Harvest the produced `fruits` of one of `your` fully grown `alive` `trees`.
      */
@@ -111,10 +136,44 @@ contract Seed is ERC721 {
      * Move one of `your` `alive` `trees` to another one of `your` `land`.
      * @dev Similar to {plant}
      */
-    function move() external {}
+    function move(uint256 _treeId, uint256 _newLandId) external {
+        // Check if the token exists, if the sender owns the seed. Same thing for the new land
+        if (ownerOf(_treeId) != msg.sender || land.ownerOf(_newLandId) != msg.sender) revert Unauthorized();
+        // Check if it is not a seed or dead
+        TreeState treeState = getState(_treeId);
+        if (treeState == TreeState.SEED || treeState == TreeState.DEAD) revert InvalidStateError(treeState);
+        // Update the tree state
+        treeDatas[_treeId].landId = _newLandId;
+        // Event
+        emit Planted(_treeId, _newLandId);
+    }
     /**
-     * Burn one of `your` `tree` and empty the corresponding `land`.
+     * Burn one of `your` `trees` and empty the corresponding `land`.
      * @dev See {ERC721-_burn}
      */
-    function burn() external {}
+    function burn(uint256 _treeId) external {
+        // Check if the token exists and if the sender owns the seed
+        if (ownerOf(_treeId) != msg.sender) revert Unauthorized();
+        // TODO: Should we check the tree state ? 
+        delete treeDatas[_treeId];
+        _burn(_treeId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 }
