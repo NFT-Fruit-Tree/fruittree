3963ced0bde59f83ab697925728a2ef2d2ae2122
Ganesh Tiwari <ganesht049@gmail.com>
2021-09-29 20:40:16 +0530

    created nav component

---
+++ packages/react-app/src/components/treeui/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./treeui.scss";
+
+const NavBar = () => {
+  return (
+    <a href="/treeui">
+      <nav className="flex text-2xl font-bold navbg text-white p-4">NFT Fruit Tree</nav>;
+    </a>
+  );
+};
+
+export default NavBar;
